24f63815fed05f314288edb07857a52b5b1bbf2d
Fernando Mora Antequera <luis.iarmolen@gmail.com>
2024-06-05 20:59:50 -0500

    my changes

--- el.py
+++ el.py
@@ -3,6 +3,9 @@ import pandas as pd
 arraez = pd.read_csv('savant_data.csv')
 
 arraez['count'] = arraez['balls'].astype(str) + '-' + arraez['strikes'].astype(str)
+arraez['pfx_z_inches'] = arraez['pfx_z'] * 12
+arraez['pfx_x_inches'] = arraez['pfx_x'] * -12
+
 arraez.drop('pitch_type', axis=1, inplace=True)
 # Move the "pitch_name" column to the first position
 cols = arraez.columns.tolist()
