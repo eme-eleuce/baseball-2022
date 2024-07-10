f5c7bc3f84a939ec636dedb568d3e00fb62e25aa
Fernando Mora Antequera <luis.iarmolen@gmail.com>
2024-07-10 13:43:29 -0500

    my changes

--- el.py
+++ el.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-arraez = pd.read_csv('savant_data.csv')
+arraez = pd.read_csv('arraez-full-2023.csv')
 
 arraez['count'] = arraez['balls'].astype(str) + '-' + arraez['strikes'].astype(str)
 arraez['pfx_z_inches'] = arraez['pfx_z'] * 12
@@ -12,4 +12,4 @@ cols = arraez.columns.tolist()
 cols.insert(0, cols.pop(cols.index('pitch_name')))
 df = arraez[cols]
 # Print the updated DataFrame
-df.to_csv('arraez-2022.csv', index=False)
+df.to_csv('arraez-2023-last.csv', index=False)
